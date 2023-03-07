f8d0d879e12b1f8a89103cbf92bb390e69c91b15
alikerock <alikerock@gmail.com>
2023-03-07 10:16:15 +0900

    cart insert create

--- cart_insert.php
+++ cart_insert.php
@@ -1,12 +1,45 @@
 <?php
-    session_start();
-    ini_set('display_errors',1);
 
-    include $_SERVER['DOCUMENT_ROOT']."/inc/dbcon.php";
+session_start();
+ini_set('display_errors',1);
 
-    $pid = $_POST['pid'];
-    $opts =  $_POST['opts'];
-    $cnt =  $_POST['cnt'];
-    $ssid = session_id();
-    $userid = $_SESSION['UID'];
+include $_SERVER['DOCUMENT_ROOT']."/inc/dbcon.php";
 
+$pid = $_POST['pid'];
+$opts =  $_POST['opts'];
+$cnt =  $_POST['cnt'];
+$ssid = session_id();
+$userid = $_SESSION['UID'];
+
+$query = "SELECT cartid from cart where ssid='".$ssid."', and pid = '".$pid."'";
+$result = $mysqli ->query($query) or die("query error =>".$mysqli_error);
+
+$rs = $result -> fetch_object();
+if($rs->cartid){
+    $sql = "UPDATE cart set cnt='".$cnt."', options='".$opts."' where ssid='".$ssid."', and pid = '".$pid."'";
+    $result = $mysqli ->query($sql) or die("query error =>".$mysqli_error);
+} else{
+    $sql = "INSERT INTO  `cart`
+    (`pid`,
+    `userid`,
+    `ssid`,
+    `options`,
+    `cnt`,
+    `regdate`)
+    VALUES
+    ('".$pid."',
+    '".$userid."',
+    '".$ssid."',
+    '".$opts."',
+    '".$cnt."',
+    now())";
+    $result = $mysqli ->query($sql) or die("query error =>".$mysqli_error);
+}
+if($result){
+    $data = array('result' => 'ok');
+} else{
+    $data = array('result' => 'fail');
+}
+echo json_encode($data);
+
+?>
